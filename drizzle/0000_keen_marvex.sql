ec539c047f2adaf37849957078c1ecb0b84db656
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-20 23:27:34 +0530

    chore(db): add initial schema and migrations
    - Add users and roles tables
    - Configure drizzle migrations

---
+++ drizzle/0000_keen_marvex.sql
@@ -0,0 +1,55 @@
+CREATE TYPE "public"."gender" AS ENUM('Male', 'Female');--> statement-breakpoint
+CREATE TYPE "public"."leave_type" AS ENUM('First half', 'Second half', 'Full day');--> statement-breakpoint
+CREATE TYPE "public"."status" AS ENUM('Pending', 'Approved', 'Rejected');--> statement-breakpoint
+CREATE TABLE "leave_requests" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid,
+	"start_date" timestamp NOT NULL,
+	"end_date" timestamp NOT NULL,
+	"request_to_id" uuid,
+	"leave_type" "leave_type" NOT NULL,
+	"reason" text NOT NULL,
+	"status" "status" DEFAULT 'Pending',
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "roles" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "roles_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar NOT NULL,
+	"priority" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user_leaves" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid,
+	"total_leave" integer NOT NULL,
+	"available_leave" integer NOT NULL,
+	"used_leave" integer NOT NULL,
+	"academic_year" varchar NOT NULL,
+	"total_working_days" integer NOT NULL,
+	"attendance_percentage" numeric NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar NOT NULL,
+	"password" varchar NOT NULL,
+	"gender" "gender" NOT NULL,
+	"image" varchar NOT NULL,
+	"phone" varchar NOT NULL,
+	"address" varchar,
+	"department" varchar,
+	"role_id" integer,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_request_to_id_users_id_fk" FOREIGN KEY ("request_to_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_leaves" ADD CONSTRAINT "user_leaves_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;
