798fe3dd8edb05f517d54c95e424872854114d69
sankitdev <ankitsingh635101@gmail.com>
2025-01-20 17:50:42 +0530

    feat: setup express server

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+import { PORT } from "./config/config.ts";
+
+const SERVERPORT = PORT || 3000;
+
+app.listen(SERVERPORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
