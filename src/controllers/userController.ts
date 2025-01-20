6adaf77a26c700b0ec3f897a4a856c1ab542822d
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-20 23:30:07 +0530

    feat: add login logic to controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { usersTable } from "./../db/schema";
 import { Request, Response } from "express";
-import { db } from "../db";
-import bcrypt from "bcrypt";
+import { db } from "../db/index";
+import { hash, compare } from "bcrypt";
 import { eq } from "drizzle-orm";
 export const studentRegister = async (req: Request, res: Response) => {
   try {
@@ -16,7 +16,7 @@ export const studentRegister = async (req: Request, res: Response) => {
       department,
       roleId,
     } = req.body;
-    const hashPass = await bcrypt.hash(password, 10);
+    const hashPass = await hash(password, 10);
     const user: typeof usersTable.$inferInsert = {
       name,
       email,
@@ -37,5 +37,27 @@ export const studentRegister = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-  } catch (error) {}
+    if (!email || !password) {
+      return res.status(400).json({ error: "Enter credentials" });
+    }
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+    if (user.length === 0) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const isValidPassword = await compare(password, user[0].password);
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    return res
+      .status(200)
+      .json({
+        message: "Login successful",
+        user: { id: user[0].id, email: user[0].email },
+      });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
