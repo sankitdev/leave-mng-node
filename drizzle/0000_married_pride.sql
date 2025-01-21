a93a281558aff9b0182e6296115f54d7d2964fa2
sankitdev <ankitsingh635101@gmail.com>
2025-01-21 16:36:44 +0530

    feat: Update database schema with new enum types and modify foreign key constraints

--- drizzle/0000_married_pride.sql
+++ drizzle/0000_married_pride.sql
@@ -1,6 +1,7 @@
-CREATE TYPE "public"."gender" AS ENUM('Male', 'Female');--> statement-breakpoint
-CREATE TYPE "public"."leave_type" AS ENUM('First half', 'Second half', 'Full day');--> statement-breakpoint
-CREATE TYPE "public"."status" AS ENUM('Pending', 'Approved', 'Rejected');--> statement-breakpoint
+CREATE TYPE "public"."department" AS ENUM('cs', 'mechanical');--> statement-breakpoint
+CREATE TYPE "public"."gender" AS ENUM('male', 'female');--> statement-breakpoint
+CREATE TYPE "public"."leave_type" AS ENUM('first_half', 'second_half', 'full_day');--> statement-breakpoint
+CREATE TYPE "public"."status" AS ENUM('pending', 'approved', 'rejected');--> statement-breakpoint
 CREATE TABLE "leave_requests" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid,
@@ -9,7 +10,7 @@ CREATE TABLE "leave_requests" (
 	"request_to_id" uuid,
 	"leave_type" "leave_type" NOT NULL,
 	"reason" text NOT NULL,
-	"status" "status" DEFAULT 'Pending',
+	"status" "status" DEFAULT 'pending',
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
@@ -17,7 +18,8 @@ CREATE TABLE "leave_requests" (
 CREATE TABLE "roles" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "roles_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"name" varchar NOT NULL,
-	"priority" integer NOT NULL
+	"priority" integer NOT NULL,
+	CONSTRAINT "roles_name_unique" UNIQUE("name")
 );
 --> statement-breakpoint
 CREATE TABLE "user_leaves" (
@@ -42,14 +44,14 @@ CREATE TABLE "users" (
 	"image" varchar NOT NULL,
 	"phone" varchar NOT NULL,
 	"address" varchar,
-	"department" varchar,
+	"department" "department",
 	"role_id" integer,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now(),
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
-ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_request_to_id_users_id_fk" FOREIGN KEY ("request_to_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_leaves" ADD CONSTRAINT "user_leaves_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "leave_requests" ADD CONSTRAINT "leave_requests_request_to_id_users_id_fk" FOREIGN KEY ("request_to_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_leaves" ADD CONSTRAINT "user_leaves_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE set null ON UPDATE no action;
---
+++ drizzle/0001_mushy_lorna_dane.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ALTER COLUMN "department" DROP NOT NULL;
