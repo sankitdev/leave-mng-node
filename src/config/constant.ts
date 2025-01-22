b3d2231b53b3838bb695a4b712112c54bba1d38c
sankitdev <ankitsingh635101@gmail.com>
2025-01-22 12:08:51 +0530

    feat: Add student leave management constants and apply leave route with authentication

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -4,3 +4,12 @@ export const roles = [
   { name: "staff", priority: 3 },
   { name: "student", priority: 4 },
 ];
+
+export const student = {
+  totalLeave: 20,
+  availableLeave: 20,
+  usedLeave: 0,
+  academicYear: new Date().getFullYear(),
+  totalWorkingDays: 220,
+  attendancePercentage: 100,
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import e from "express";
 import {
   getUsers,
+  leaveRequest,
   loginUser,
   studentRegister,
 } from "../controllers/userController";
@@ -9,5 +10,6 @@ const user = e.Router();
 
 user.post("/register", studentRegister);
 user.post("/login", loginUser);
-user.get("/users", auth, getUsers);
+user.get("/users", auth(["admin"]), getUsers);
+user.post("/apply-leave", auth(["student"]), leaveRequest);
 export default user;
