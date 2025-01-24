fb9bce08c294913ad08583ad477f188c58780c42
sankitdev <ankitsingh635101@gmail.com>
2025-01-24 16:45:01 +0530

    feat: Add CORS support to the application and update package.json dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import router from "./routes";
+import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", router);
