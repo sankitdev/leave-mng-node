965e2c80276305928bfa964373568700ee314bad
Ankit Singh <ankitsingh635101@gmail.com>
2025-01-26 19:06:33 +0530

    refactor: Remove unnecessary whitespace in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import app from "./app";
 import { PORT } from "./config/config.ts";
-
 const SERVERPORT = PORT || 3000;
-
 app.listen(SERVERPORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
