7fd2b6fc22da8ff40f899068815ecc9f315a5dc4
sankitdev <ankitsingh635101@gmail.com>
2025-01-29 12:50:07 +0530

    feat: Refactor config to destructure environment variables for cleaner access

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,11 @@
 import { configDotenv } from "dotenv";
 configDotenv();
-export const DATABASE_URL = process.env.DATABASE_URL;
-export const PORT = process.env.PORT;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
+
+export const {
+  DATABASE_URL,
+  PORT,
+  JWT_SECRET,
+  JWT_EXPIRATION,
+  ADMIN_EMAIL,
+  ADMIN_PASSWORD,
+} = process.env;
