9f093b70d602a45399c80b46a72e18fcc9b8252b
sankitdev <ankitsingh635101@gmail.com>
2025-01-29 12:48:50 +0530

    feat: Clean up database migration files and update .gitignore for drizzle

--- drizzle/0001_mushy_lorna_dane.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "department" DROP NOT NULL;
--- drizzle/0002_gigantic_preak.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "leave_requests" ALTER COLUMN "start_date" SET DATA TYPE date;--> statement-breakpoint
-ALTER TABLE "leave_requests" ALTER COLUMN "end_date" SET DATA TYPE date;--> statement-breakpoint
-ALTER TABLE "user_leaves" ALTER COLUMN "used_leave" SET DEFAULT 0;--> statement-breakpoint
-ALTER TABLE "user_leaves" DROP COLUMN "total_working_days";
