33cca69143ced50747033203e7e3ce4e934c867e
sankitdev <ankitsingh635101@gmail.com>
2025-01-30 11:56:43 +0530

    feat: Update leave retrieval route to allow access for admin, hod, and staff roles

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -17,5 +17,9 @@ student.get("/staff/:department", auth(["student"]), getTeacherForLeave);
 student.post("/apply-leave", auth(["student"]), applyLeave);
 student.get("/leaves/requests", auth(["student"]), getPersonalLeaveRequests);
 student.get("/leaves/balance", auth(["student"]), getLeaveBalance);
-student.get("/leaves/:department", auth(["student"]), getLeavesByDepartment);
+student.get(
+  "/leaves/:department",
+  auth(["student", "admin", "hod", "staff"]),
+  getLeavesByDepartment
+);
 export default student;
